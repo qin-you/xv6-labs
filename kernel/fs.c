4e2f8c50b65f25fc71590ae4e695542f4f3745d5
qin-you <mg20150006@smail.nju.edu.cn>
2023-07-13 16:35:23 +0800

    fs lab

--- kernel/fs.c
+++ kernel/fs.c
@@ -377,16 +377,18 @@ iunlockput(struct inode *ip)
 static uint
 bmap(struct inode *ip, uint bn)
 {
-  uint addr, *a;
-  struct buf *bp;
+  uint addr, *a, *b;
+  struct buf *bp, *ininbp;
 
+  /* internal block number in 0 ~ 10*/
   if(bn < NDIRECT){
     if((addr = ip->addrs[bn]) == 0)
       ip->addrs[bn] = addr = balloc(ip->dev);
     return addr;
   }
-  bn -= NDIRECT;
 
+  /*internal block number belongs to one-level block page*/
+  bn -= NDIRECT;
   if(bn < NINDIRECT){
     // Load indirect block, allocating if necessary.
     if((addr = ip->addrs[NDIRECT]) == 0)
@@ -401,6 +403,31 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
+  /* internal block number belongs to two-level block page */
+  bn -= NINDIRECT;
+  if (bn < NININDIRECT) {
+    if ((addr = ip->addrs[NDIRECT+1]) == 0) {
+      ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);  // alloc block for 1st block page itself
+    }
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if ((addr = a[bn/NINDIRECT]) == 0) {          // entry in first block page
+      a[bn/NINDIRECT] = addr = balloc(ip->dev);   // alloc block for the second block page itself
+      log_write(bp);
+    }
+    brelse(bp);
+
+    ininbp = bread(ip->dev, addr);
+    b = (uint*)ininbp->data;
+    if ((addr = b[bn % NINDIRECT]) == 0) {
+      b[bn % NINDIRECT] = addr = balloc(ip->dev);
+      log_write(ininbp);
+    }
+    brelse(ininbp);
+
+    return addr;
+  }
+
   panic("bmap: out of range");
 }
 
@@ -409,9 +436,9 @@ bmap(struct inode *ip, uint bn)
 void
 itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i, j, k;
+  struct buf *bp, *inbp;
+  uint *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -430,6 +457,28 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if (ip->addrs[NDIRECT+1]) {
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);  // read 1st block page table itself
+    a = (uint*)bp->data;
+    for (j = 0; j < NINDIRECT; j++) {
+      if (a[j]) {
+        inbp = bread(ip->dev, a[j]);    // read second block page table itself
+        b = (uint*)inbp->data;
+        for (k = 0; k < NINDIRECT; k++) {
+          if (b[k]) {
+            bfree(ip->dev, b[k]);
+          }
+        brelse(inbp);
+        bfree(ip->dev, a[j]);
+        }
+      }
+
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
   }
 
   ip->size = 0;
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -291,6 +291,7 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
+  int r;
 
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
@@ -315,6 +316,34 @@ sys_open(void)
       return -1;
     }
   }
+
+  /* find target file if this is a sympolink and not set O_NOFOLLOW */
+  /* i.e. find correct ip*/
+  int depth = 0;
+  while (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+    char ktarget[MAXPATH];
+    memset(ktarget, 0, MAXPATH);
+    if ((r = readi(ip, 0, (uint64)ktarget, 0, MAXPATH)) < 0) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    iunlockput(ip);
+    if ((ip = namei(ktarget)) == 0) {     // get inode according to name
+      // target file not exist
+      end_op();
+      return -1;
+    }
+
+    ilock(ip);
+    depth++;
+    if (depth > 10) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  }
+
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
@@ -483,4 +512,46 @@ sys_pipe(void)
     return -1;
   }
   return 0;
+}
+
+uint64 
+sys_symlink(void)
+{
+  char kpath[MAXPATH], ktarget[MAXPATH];
+  memset(kpath, 0, MAXPATH);
+  memset(ktarget, 0, MAXPATH);
+  struct inode *ip;
+  int n, r;
+
+  if ((n = argstr(0, ktarget, MAXPATH)) < 0)
+    return -1;
+
+  if ((n = argstr(1, kpath, MAXPATH)) < 0)
+    return -1;
+
+  int ret = 0;
+
+  begin_op();
+
+  if ((ip = namei(kpath)) != 0) {
+    // symlink already exists
+    ret = -1;
+    goto final;
+  }
+
+  ip = create(kpath, T_SYMLINK, 0, 0);
+  if (ip == 0) {
+    ret = -1;
+    goto final;
+  }
+
+  // write ktarget's content to inode's blocks specified by ip->addrs
+  if ((r = writei(ip, 0, (uint64)ktarget, 0, MAXPATH)) < 0) {
+    ret = -1;
+  }
+  iunlockput(ip);
+
+final:
+  end_op();
+  return ret;
 }
